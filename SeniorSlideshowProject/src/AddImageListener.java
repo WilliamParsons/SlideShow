55c5528459af63292fd8c58fd7bbb03f10d2ac4d
WilliamParsons <wdp0005@uah.edu>
2017-03-05 11:45:56 -0600

    add the image to the slideshow

--- SeniorSlideshowProject/src/AddImageListener.java
+++ SeniorSlideshowProject/src/AddImageListener.java
@@ -1,11 +1,14 @@
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.util.Vector;
-
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
+
+import Slides.SlideShowStateMachine;
+import Slides.SlideState;
+
+
 public class AddImageListener implements ActionListener {
 
 	private static final int FILES_AND_DIRECTORIES = 2;
@@ -15,22 +18,25 @@ public class AddImageListener implements ActionListener {
 		JFileChooser fc = new JFileChooser();
 		fc.setFileFilter(new FileTypeFilter(".jpg", "image file"));
 		fc.setFileSelectionMode(FILES_AND_DIRECTORIES);
+		SlideShowStateMachine mySlideShow = SlideShowStateMachine.getInstance();
 		int result = fc.showDialog(null, "Add Image");
 		if (result == JFileChooser.APPROVE_OPTION){
 			String selectedFilePath = fc.getSelectedFile().getPath();
 			if (selectedFilePath.contains(".jpg")){
 				ImageIcon icon = new ImageIcon(selectedFilePath);
+				SlideState mySlide = new SlideState(icon);
+				mySlideShow.addSlide(mySlide);
 			}
 			else{
 				makeListOfImages imageVector = new makeListOfImages();
 				Vector imageFileVector = imageVector.listFilesAndFilesSubDirectories(selectedFilePath);
 				for (int i = 0; i < imageFileVector.size(); i++){
 					String filePath = (String) imageFileVector.elementAt(i);
-					ImageIcon icon = new ImageIcon(selectedFilePath);
+					ImageIcon icon = new ImageIcon(filePath);
+					SlideState mySlide = new SlideState(icon);
+					mySlideShow.addSlide(mySlide);
 				}
 			}
-			System.out.println("getCurrentDirectory(): " + fc.getCurrentDirectory());
-			System.out.println("getSelectedFile(): " + fc.getSelectedFile());
 		}	
 	}
 }
