1eb88b32a2421d33b985f65091cf79d7ad1b2d3f
emd0008 <emd0008@uah.edu>
2017-04-17 17:52:08 -0500

    Update

--- SeniorSlideshowProject/src/Slides/SlideState.java
+++ SeniorSlideshowProject/src/Slides/SlideState.java
@@ -6,49 +6,49 @@ import javax.swing.ImageIcon;
 
 public class SlideState implements Serializable {
 	
-	public enum Transition{ NONE, LEFT, RIGHT, UP, DOWN, CROSSFADE };
+	public enum Transition{ NONE, LEFT, RIGHT, UP, DOWN, CROSSFADE };		//Enumerated Types: NONE, LEFT, RIGHT, UP, DOWN, CROSSFADE
 	
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;						//Serial ID
 	
 	private Transition transitionType;
 	private ImageIcon icon;
 	private double transitionTime;
 	
-	public SlideState(ImageIcon icon)
+	public SlideState(ImageIcon icon)										//Function to set SlideState to passed-in ImageIcon
 	{
-		this.icon = icon;
-		transitionType = Transition.NONE;
+		this.icon = icon;													//Current icon is set to passed-in icon
+		transitionType = Transition.NONE;									//Set the transitiion type to NONE
 	}
 	
-	public SlideState(ImageIcon icon, Transition type)
+	public SlideState(ImageIcon icon, Transition type)						//Function to set SlideState with icon and transition
 	{
-		this.icon = icon;
-		transitionType = type;
+		this.icon = icon;													//Current icon is set to passed-in icon
+		transitionType = type;												//Current type is set to passed-in type
 	}
 	
-	public void setTransitionType(Transition type)
+	public void setTransitionType(Transition type)							//Function to set transition type
 	{
-		transitionType = type;
+		transitionType = type;												//Current type is set to passed-in type
 	}
 	
-	public void setTransitionTime(double time)
+	public void setTransitionTime(double time)								//Function to set the transition time
 	{
-		transitionTime = time;
+		transitionTime = time;												//Current time is set to passed-in time
 	}
 	
-	public ImageIcon getIcon()
-	{
-		return this.icon;
+	public ImageIcon getIcon()												//Function to get icon
+	{	
+		return this.icon;													//Return current icon
 	}
 	
-	public Transition getTransition()
+	public Transition getTransition()										//Function to get transition
 	{
-		return transitionType;
+		return transitionType;												//Return current transition
 	}
 	
-	public double getTransitionTime()
+	public double getTransitionTime()										//Function to get transition time
 	{
-		return transitionTime;
+		return transitionTime;												//Return current transition time
 	}
 	
 }
