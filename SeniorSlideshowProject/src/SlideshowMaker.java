fb767c74db2fe858ae4b33eb9ebbe41084ead127
Jinke <jy0015@uah.edu>
2017-04-18 16:02:45 -0500

    remove useless code

--- SeniorSlideshowProject/src/SlideshowMaker.java
+++ SeniorSlideshowProject/src/SlideshowMaker.java
@@ -740,9 +740,4 @@ public class SlideshowMaker extends JFrame implements Observer {
 								// this function
 		System.out.print("SlideshowMaker: soundtrack table is changed\n");
 	}
-
-	public SoundTrack getSoundTrack() {
-		// TODO Auto-generated method stub
-		return this.soundTrack;
-	}
 }
